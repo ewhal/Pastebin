8c153440cf415c6b5bb0ecf0f246db0cc2323780
Eliot Whalan <ewhal@pantsu.cat>
2017-02-02 16:53:51 +1000

    Add accounts schema

--- database.sql
+++ database.sql
@@ -5,5 +5,13 @@ CREATE TABLE `pastebin` (
   `data` longtext,
   `delkey` char(40) default NULL,
   `expiry` int,
+  `userid` int,
+  PRIMARY KEY (`id`)
+);
+
+CREATE TABLE `accounts` (
+  `id` varchar(30) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `pass` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 );
